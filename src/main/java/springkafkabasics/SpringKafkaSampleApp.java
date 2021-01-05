68e33784e94b1b28019bb8bb790cfdb8918e6685
mrayandutta21 <mrayandutta21@gmail.com>
2021-01-05 17:03:59 +0000

    Added kafka producer details

---
+++ src/main/java/springkafkabasics/SpringKafkaSampleApp.java
@@ -0,0 +1,32 @@
+package springkafkabasics;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import springkafkabasics.producer.KafkaProducer;
+
+@SpringBootApplication
+public class SpringKafkaSampleApp implements CommandLineRunner
+{
+	private static Logger logger = LoggerFactory.getLogger(SpringKafkaSampleApp.class.getName());
+	
+	@Autowired
+	KafkaProducer kafkaProducer;
+
+		    public static void main(String[] args) {
+		    	logger.info("STARTING THE APPLICATION");
+		    	SpringApplication.run(SpringKafkaSampleApp.class, args);
+		        logger.info("APPLICATION FINISHED");
+		    }
+
+			@Override
+			public void run(String... args) throws Exception {
+				kafkaProducer.sendMessage("test");
+				
+			}
+
+}
---
+++ src/main/java/springkafkabasics/producer/KafkaProducerConfig.java
@@ -0,0 +1,49 @@
+package springkafkabasics.producer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+
+@Configuration
+public class KafkaProducerConfig {
+	
+	@Value(value = "${spring.kafka.bootstrap-servers}")
+    private String bootstrapAddress;
+	
+	//private static final String TRUSTSTORE_JKS = "/var/private/ssl/kafka.client.truststore.jks"; 
+	private static final String SASL_PROTOCOL = "SASL_SSL"; 
+	private static final String SCRAM_SHA_256 = "SCRAM-SHA-256"; 
+	private final String jaasTemplate = "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"%s\" password=\"%s\";"; 
+	private final String prodJaasCfg = String.format(jaasTemplate, "cyy3wd7r", "eAQPX5G290PkZ5CKo6drJKvqHqO6FA66");
+
+	 @Bean 
+	 public ProducerFactory<String, String> producerFactory() { 
+	   final Map<String, Object> configProps = new HashMap<>(); 
+	   configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress); 
+	   configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class); 
+	   configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class); 
+	   //configProps.put(ProducerConfig.ACKS_CONFIG, "all"); 
+	   //configProps.put(ProducerConfig.CLIENT_ID_CONFIG, "cid1"); 
+	   configProps.put("sasl.mechanism", SCRAM_SHA_256); 
+	   configProps.put("sasl.jaas.config", prodJaasCfg); 
+	   configProps.put("security.protocol", SASL_PROTOCOL); 
+	   //configProps.put("ssl.truststore.location", TRUSTSTORE_JKS); 
+	   //configProps.put("ssl.truststore.password", "password"); 
+	   //configProps.put("ssl.endpoint.identification.algorithm", ""); 
+	   return new DefaultKafkaProducerFactory<>(configProps); 
+	 }
+
+	@Bean
+	public KafkaTemplate<String, String> kafkaTemplate() {
+		return new KafkaTemplate<>(producerFactory());
+	}
+}
