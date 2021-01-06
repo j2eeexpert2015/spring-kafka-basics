e1152c10aa1fd38d952cfd9624c41c941194c615
mrayandutta21 <mrayandutta21@gmail.com>
2021-01-06 16:34:56 +0000

    Added comments

--- src/main/java/springkafkabasics/producer/KafkaProducer.java
+++ src/main/java/springkafkabasics/producer/KafkaProducer.java
@@ -9,7 +9,13 @@ import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
-
+/**
+ * 
+ * 1.To enable the Kafka producer kafkaTemplate() and producerFactory() methods should be implemented in KafkaConfig class. 
+ * 2.The kafkaTemplate() will return a new kafkaTemplate based on the configuration defined in producerFactory(). 
+ * 3.This KafkaTemplate sends messages to Kafka topic 
+ *
+ */
 @Component
 public class KafkaProducer {
 	
